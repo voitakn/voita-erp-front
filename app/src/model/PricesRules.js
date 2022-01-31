414898520ae666c514cb7b50f8002f0f11780e83
EKarakay <satsystem97@gmail.com>
2022-01-31 17:10:37 +0300

    fix select affected prices rules

--- app/src/model/PricesRules.js
+++ app/src/model/PricesRules.js
@@ -31,7 +31,7 @@ Ext.define('Erp.model.PricesRules', {
         {
             name: 'affect_price',
             calculate(data) {
-                return data.parent_id ? User.rulesObj[data.parent_id].title : 'Not';
+                return data.parent_id && User.rulesObj[data.parent_id] ? User.rulesObj[data.parent_id].title : 'Not';
             }
         },
     ],
