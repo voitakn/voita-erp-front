ab6512820cf53cb5156248af0568b53c5b304826
Constantin Voita <voitaerp@gmail.com>
2022-03-30 08:17:37 +0100

    Changed develop configs

---
+++ resources/data/config.js
@@ -0,0 +1,9 @@
+CfgData = {
+  "authUrl": "https://auth.voita.eu/auth/signin",
+  "logoutUrl": "/user/logout",
+  "clsApi": {
+    "domain": "voita.local",
+    "protocol": "http",
+    "port": ":8004"
+  }
+};
